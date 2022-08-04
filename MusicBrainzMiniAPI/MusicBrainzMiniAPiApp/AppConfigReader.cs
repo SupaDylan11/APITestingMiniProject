dad41783481a9e51c5dbc1b9d148d5259886a9a1
JonOfScience <jdcrofts31@gmail.com>
2022-08-04 15:55:21 +0100

    DTO IResponse and AppConfigReader complete

---
+++ MusicBrainzMiniAPI/MusicBrainzMiniAPiApp/AppConfigReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace MusicBrainzMiniAPiApp;
+
+public static class AppConfigReader
+{
+    public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
+}
